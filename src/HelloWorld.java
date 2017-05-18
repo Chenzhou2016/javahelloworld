6da0f616717e192ee643a310bb3ed1bdf5eee60c
Ubuntu <helen@Ubuntu1604Docker.qb4uj31ypecuhb2r4qbsaf4nih.ix.internal.cloudapp.net>
2017-05-18 05:14:03 +0000

    change the output message

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld{
 	public static void main (String [] args){
-		System.out.println("Hello, Helen Chen!");
+		System.out.println("This is the Greeting from Docker, Helen Chen!");
 	}
 }
